160103bd67c62cf2e513722fc9a5d4ecb9648427
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-09-04 16:57:33 -0500

    deleted folder messing with handlebars and made test app mvc

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
 
-app.listen(PORT, function () {
+const hbs = exphbs.create({ });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('home')
+});
+
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 })
