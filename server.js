81e67255e7551921efcfa8829f0f234ba8f042e3
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-09-05 12:52:39 -0500

    added new view and controller now handles requests

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const homepageRouter = require('./controllers/homepage');
+// const routes = require('./controllers');
 
+// start the express app
 const app = express();
+//define the port
 const PORT = process.env.PORT || 3000;
 
+// create handlebars
 const hbs = exphbs.create({ });
 
+//regiter it in the express server
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// use routet
+app.use('/', homepageRouter);
+//middleware for server
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.render('home')
-});
-
+//app listens
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 })
