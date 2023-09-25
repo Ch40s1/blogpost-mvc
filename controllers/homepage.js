7d99af020ed9b89fe8b3f2a7379bf3dbf48e8571
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-09-25 14:04:57 -0500

    profile is now only accessible with authentication only

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,5 +1,6 @@
 const { BlogPost, User } = require('../models');
 const router = require('express').Router();
+const withAuth = require('../utils/auth');
 
 // Get basic homepage with blog post data
 router.get('/', async (req, res) => {
@@ -29,5 +30,23 @@ router.get('/', async (req, res) => {
 router.get('/login', (req, res) => {
   res.render('login')
 })
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+    });
 
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true,
+      user_name: req.session.user_name,
+      user_email: req.session.user_email,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
